3f716f27ba34c7ab9e53512b2d971c7785873cb3
vdanalachi <113927916+vdanalachi@users.noreply.github.com>
2022-09-19 17:44:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('~airplane My Parents New Healthy Diner')
+streamlit.title(':smile: My Parents New Healthy Diner')
 streamlit.header('Breakfast Favories')
 streamlit.text('Omega 3 &Blueberry ouatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
