b1cdfd65156789a4b47fa51a648d4046bc1f3822
vdanalachi <113927916+vdanalachi@users.noreply.github.com>
2022-09-23 12:19:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,10 @@ streamlit.header('The fruit load list contains')
 streamlit.dataframe(my_data_rows)
 
 
+fruit_choice1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', fruit_choice1)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{}".format(fruit_choice))
+
+
+
